a273f6f9705b1c40328d95a5b8981916256a0259
jiyeanlee <gui.ljy@zempublic.co.kr>
2023-03-30 17:27:25 +0900

    중간 업로드

--- my-app/src/main/BottomBar.tsx
+++ my-app/src/main/BottomBar.tsx
@@ -12,7 +12,7 @@ function BottomBar() {
         <div className='pre-btn'>승부예측</div>
       </Affix>
       <Affix offsetBottom={0} onChange={affixed => console.log(affixed)}>
-        <Button></Button>
+        <Button shape='circle' className='bottom-btn'><span>6</span><span className='bottom-btn-date'>12.15</span></Button>
       </Affix>
       <Affix offsetBottom={0} onChange={affixed => console.log(affixed)}>
         <div className='his-btn'>베팅내역</div>
