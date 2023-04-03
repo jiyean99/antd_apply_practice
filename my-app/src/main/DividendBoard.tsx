66f8df5ba58e32c7f012f7a1e3c8a67497ca6065
jiyeanlee <gui.ljy@zempublic.co.kr>
2023-04-03 18:24:21 +0900

    antd radio disabled 시도

--- my-app/src/main/DividendBoard.tsx
+++ my-app/src/main/DividendBoard.tsx
@@ -3,6 +3,9 @@ import '../style/style.css';
 import { Button, Radio } from 'antd';
 import { Typography } from 'antd';
 import { Collapse } from 'antd';
+// import { useState } from "react"
+import React, { useState } from "react";
+import { RadioChangeEvent } from 'antd/lib/radio';
 
 const { Panel } = Collapse;
 
@@ -15,7 +18,20 @@ const text = `
 
 const { Text } = Typography;
 
+
 function DividendBoard() {
+    
+    const [disabled, setDisabled] = useState([false, false, false, false, false, false, false]);
+    
+    // // 기존 e가 any타입으로 지정되어있어 e를 명시적으로 지정해주면서 추가적으로 RadioChangeEvent를 import 하였습니다
+    const onChange = (e: RadioChangeEvent) => {
+      if (e.target.value === 1) {
+        setDisabled([true, true, false, false, false, false, false]);
+      } else {
+        setDisabled([false, false, false, false, false, false, false]);
+      }
+    };
+
   return (
     <div className="DividendBoard">
         <Button 
@@ -49,32 +65,32 @@ function DividendBoard() {
                         <Text className='league-table-r'>SKA 상트페테르부르크</Text>
                     </Text>
                 </div>
-                <Radio.Group buttonStyle="solid" className='d-b-btn-group'>
-                    <Radio.Button value="a" className='d-b-btn'>
+                <Radio.Group  onChange={onChange} buttonStyle="solid" className='d-b-btn-group'>
+                    <Radio.Button value="1" className='d-b-btn' disabled={disabled[0]} >
                         <span>홈 승</span><span className='d-b-num'>1.93</span>
                     </Radio.Button>
-                    <Radio.Button value="b" className='d-b-btn'>
+                    <Radio.Button value="2" className='d-b-btn' disabled={disabled[1]} >
                         <span>무</span><span className='d-b-num'>1.93</span>
                     </Radio.Button>
-                    <Radio.Button value="c" className='d-b-btn'>
+                    <Radio.Button value="3" className='d-b-btn' disabled={disabled[2]} >
                         <span className='d-b-num'>1.93</span><span>원정 승</span>
                     </Radio.Button>
-                    <Radio.Button value="d" className='d-b-btn'>
+                    <Radio.Button value="4" className='d-b-btn' disabled={disabled[3]} >
                         <span>홈 승</span><span className='d-b-num'>1.93</span>
                     </Radio.Button>
-                    <Radio.Button value="e" className='d-b-btn accodian'>
+                    <Radio.Button value="5" className='d-b-btn accodian'>
                         <span className='aco-position'>H</span><span className='aco-num'>0.5</span>
                     </Radio.Button>
-                    <Radio.Button value="f" className='d-b-btn'>
+                    <Radio.Button value="6" className='d-b-btn' disabled={disabled[4]} >
                         <span className='d-b-num'>1.93</span><span>원정 승</span>
                     </Radio.Button>
-                    <Radio.Button value="g" className='d-b-btn'>
+                    <Radio.Button value="7" className='d-b-btn' disabled={disabled[5]} >
                         <span>언더</span><span className='d-b-num d-b-under'>1.18</span>
                     </Radio.Button>
-                    <Radio.Button value="h" className='d-b-btn accodian'>
+                    <Radio.Button value="8" className='d-b-btn accodian'>
                         <span className='aco-position'>U/O</span><span className='aco-num'>2.5</span>
                     </Radio.Button>
-                    <Radio.Button value="i" className='d-b-btn '>
+                    <Radio.Button value="9" className='d-b-btn' disabled={disabled[6]} >
                         <span className='d-b-num d-b-over'>1.93</span><span>오버</span>
                     </Radio.Button>
                 </Radio.Group>
